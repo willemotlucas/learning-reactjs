c3851d6acea8e72260724eaca64286bef0a4e1ab
Lucas Willemot <willemotlucas@gmail.com>
2016-12-22 20:15:46 +0100

    Correcting a typo and removing ES6 sample code

--- udemy-react-webapp-course/ReactBoilerplate/app/app.jsx
+++ udemy-react-webapp-course/ReactBoilerplate/app/app.jsx
@@ -1,18 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var objOne = {
-  name: 'Lucas',
-  location: 'Blois'
-};
-
-var objTwo = {
-  age: '22',
-  ...objOne
-};
-
-console.log(objTwo);
-
 ReactDOM.render(
   <h1>Boilerplate React.js application</h1>,
   document.getElementById('app')
