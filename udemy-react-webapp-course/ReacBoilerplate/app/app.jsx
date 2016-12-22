588c72330ae40dc5a9efb3205ca5c445bc79b602
Lucas Willemot <willemotlucas@gmail.com>
2016-12-22 19:56:11 +0100

    Adding new features from ES6 with babel stage libraries

--- udemy-react-webapp-course/ReacBoilerplate/app/app.jsx
+++ udemy-react-webapp-course/ReacBoilerplate/app/app.jsx
@@ -1,6 +1,18 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var objOne = {
+  name: 'Lucas',
+  location: 'Blois'
+};
+
+var objTwo = {
+  age: '22',
+  ...objOne
+};
+
+console.log(objTwo);
+
 ReactDOM.render(
   <h1>Boilerplate React.js application</h1>,
   document.getElementById('app')
