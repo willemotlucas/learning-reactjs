26cf11427026b1980078d983b83a6324ba04681e
Lucas Willemot <willemotlucas@gmail.com>
2016-12-25 13:37:24 +0100

    Installing Foundation and jQuery

--- udemy-react-webapp-course/ReactWeather/app/app.jsx
+++ udemy-react-webapp-course/ReactWeather/app/app.jsx
@@ -13,6 +13,10 @@ var Examples = require('Examples');
  * ...
 */
 
+// Loading Foundation css styles
+require('style!css!foundation-sites/dist/foundation.min.css');
+$(document).foundation();
+
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
