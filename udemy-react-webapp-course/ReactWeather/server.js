c587de0cf08db30c3d6b8e05bf33c49c6ed68a43
Lucas Willemot <willemotlucas@gmail.com>
2016-12-25 12:04:26 +0100

    Making some change to be able to deploy to heroku

--- udemy-react-webapp-course/ReactWeather/server.js
+++ udemy-react-webapp-course/ReactWeather/server.js
@@ -2,9 +2,21 @@ var express = require('express');
 
 // Create our app
 var app = express();
-var PORT = 3000;
+// Binding to the port given by Heroku, or 3000 for development
+const PORT = process.env.PORT || 3000;
+
+// We need to redirect trafic from HTTPS to HTTP in order to use 
+// OpenWeatherMap API
+app.use(function(req, res, next){
+  if(req.headers['x-forwarded-proto'] === 'http'){
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 
 app.use(express.static('public'));
+
 app.listen(PORT, function(){
   console.log('Server is running on port ' + PORT);
 });
