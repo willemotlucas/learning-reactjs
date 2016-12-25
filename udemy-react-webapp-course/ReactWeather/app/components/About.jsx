06c90cce674298d3d845e2f13c39f1883de6cd67
Lucas Willemot <willemotlucas@gmail.com>
2016-12-25 15:10:52 +0100

    Styling About page and add content

--- udemy-react-webapp-course/ReactWeather/app/components/About.jsx
+++ udemy-react-webapp-course/ReactWeather/app/components/About.jsx
@@ -18,7 +18,15 @@ var React = require('react');
 
 var About = (props) => {
     return (
-      <h3>About page</h3>
+      <div>
+        <h1 className="text-center">About</h1>
+        <p>This is a Weather application built on React.</p>
+        <p>Here are some tools I used:</p>
+        <ul>
+          <li><a href="https://facebook.github.io/react">React</a> - This was the Javascript framework used</li>
+          <li><a href="http://openweathermap.org">Open Weather Map</a> - I used Open Weather Map to fetch weather by city name</li>
+        </ul>
+      </div>
     );
 }
 
--- udemy-react-webapp-course/ReactWeather/app/components/Examples.jsx
+++ udemy-react-webapp-course/ReactWeather/app/components/Examples.jsx
@@ -6,7 +6,7 @@ var {Link} = require('react-router');
 var Examples = (props) => {
     return (
       <div>
-        <h1 className="text-center">Examples page</h1>
+        <h1 className="text-center">Examples</h1>
         <p>Here are a few example location to try out:</p>
         <ol>
           <li><Link to="/?location=Blois">Blois, France</Link></li>
