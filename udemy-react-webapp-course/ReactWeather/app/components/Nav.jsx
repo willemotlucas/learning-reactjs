720c3f0882a4b94633b6d6eaee61dad0883563ed
Lucas Willemot <willemotlucas@gmail.com>
2016-12-25 13:51:19 +0100

    Styling navbar with Foundation and add a searchbar in the menu

--- udemy-react-webapp-course/ReactWeather/app/components/Nav.jsx
+++ udemy-react-webapp-course/ReactWeather/app/components/Nav.jsx
@@ -1,15 +1,37 @@
 var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
-var Nav = (props) => {
-  return (
-      <div>
-        <h2>Nav component</h2>
-        <IndexLink to="/">Get Weather</IndexLink>
-        <Link to="/about">About</Link>
-        <Link to="/examples">Examples</Link>
+var Nav = React.createClass({
+
+  onSearch: function(event){
+    event.preventDefault();
+
+    alert('not yet set up');
+  },
+
+  render: function(){
+    return (
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">React Weather App</li>
+            <li><IndexLink to="/">Get Weather</IndexLink></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/examples">Examples</Link></li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <form onSubmit={this.onSearch}>
+            <ul className="menu">
+              <li><input type="text" placeholder="Search weather"/></li>
+              <li><input type="submit" className="button" value="Search"/></li>
+            </ul>
+          </form>
+        </div>
       </div>
-  );
-}
+    );
+  }
+
+})
 
 module.exports = Nav;
